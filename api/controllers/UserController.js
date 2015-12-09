a02def9d41310f98c2c7a248e3cabce2c7f009cc
Scott Gress <scottmgress@gmail.com>
2015-12-09 16:25:47 -0600

    Cleanup and fixes to comments

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,44 +1,54 @@
-// Note -- we need a UserController so that blueprints will be activated for User,
-// but we don't need any custom User controller code for this app!
-
 module.exports = {
 
-	announce: function (req, res) {
+  // Create a new user and tell the world about them.
+  // This will be called every time a socket connects, so that each socket
+  // represents one user--this is so that it's easy to demonstrate inter-user
+  // communication by opening a bunch of tabs or windows.  In the real world,
+  // you'd want multiple tabs to represent the same logged-in user.
+  announce: function(req, res) {
 
-		var socketId = sails.sockets.id(req);
-		var session = req.session;
+    // Get the socket ID from the reauest
+    var socketId = sails.sockets.id(req);
 
-        // Create the session.users hash if it doesn't exist already
-        session.users = session.users || {};
+    // Get the session from the request
+    var session = req.session;
 
-        User.create({name: 'unknown', socketId: socketId}).exec(function(err, user) {
-        	if (err) {return res.serverError(err);}
+    // Create the session.users hash if it doesn't exist already
+    session.users = session.users || {};
 
-	        // Save this user in the session, indexed by their socket ID.
-	        // This way we can look the user up by socket ID later.
-	        session.users[socketId] = user;
+    User.create({
+      name: 'unknown',
+      socketId: socketId
+    }).exec(function(err, user) {
+      if (err) {
+        return res.serverError(err);
+      }
 
-	        // Subscribe the connected socket to custom messages regarding the user.
-	        // While any socket subscribed to the user will receive messages about the
-	        // user changing their name or being destroyed, ONLY this particular socket
-	        // will receive "message" events.  This allows us to send private messages
-	        // between users.
-	        User.subscribe(req, user, 'message');
+      // Save this user in the session, indexed by their socket ID.
+      // This way we can look the user up by socket ID later.
+      session.users[socketId] = user;
 
-	        // Get updates about users being created
-	        User.watch(req);
+      // Subscribe the connected socket to custom messages regarding the user.
+      // While any socket subscribed to the user will receive messages about the
+      // user changing their name or being destroyed, ONLY this particular socket
+      // will receive "message" events.  This allows us to send private messages
+      // between users.
+      User.subscribe(req, user, 'message');
 
-	        // Get updates about rooms being created
-	        Room.watch(req);
+      // Get updates about users being created
+      User.watch(req);
 
-	        // Publish this user creation event to every socket watching the User model via User.watch()
-	        User.publishCreate(user, req);
+      // Get updates about rooms being created
+      Room.watch(req);
 
-	        res.json(user);
-        	
-        });
+      // Publish this user creation event to every socket watching the User model via User.watch()
+      User.publishCreate(user, req);
+
+      res.json(user);
+
+    });
 
 
-	}
+  }
 
 };
